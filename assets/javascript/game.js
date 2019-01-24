4d09d1e8df8c086143a44317e8b009f70d7ea8b9
isai <Isai4827@4827@gmail.com>
2019-01-24 01:38:42 -0700

    add assets/javascript/game.js index.html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,38 @@
 // declare the variables
-var randomNumber = ""
+window.onload = function () {
+    var randomNumber = ""
 
-for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < 4; i++) {
 
-    // For each iteration, generate a new random number between 1 and 9.
-    var random = Math.floor(Math.random() * ) + 1;
+        // For each iteration, generate a new random number between 1 and 5.
+        var random = Math.random() * 4 + 1;
 
-    randomNumber = random + randomNumber;
+        randomNumber = random + randomNumber;
 
-}
+    }
+    document.getElementById("hiddenNumber").innerHTML = randomNumber;
+};
+$("#button-1").on("click", function () {
+    var random = math.random() * 2 + 1;
+
+    document.getElementById("number1").innerHTML = random;
+
+});
+$("#button-2").on("click", function () {
+    var random = math.random() * 2 + 1;
+
+    document.getElementById("number2").innerHTML = random;
+
+});
+$("#button-3").on("click", function () {
+    var random = math.random() * 2 + 1;
+
+    document.getElementById("number3").innerHTML = random;
+
+});
+$("#button-4").on("click", function () {
+    var random = math.random() * 2 + 1;
+
+    document.getElementById("number4").innerHTML = random;
+
+});
