d1ed84c17b648e79a4b4acf06536ed45beb03b88
isai <Isai4827@4827@gmail.com>
2019-01-23 22:45:24 -0700

    add assets/css/reset.css assets/css/styl.css assets/javascript/game.js index.html

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// declare the variables
+var randomNumber = ""
+
+for (var i = 0; i < 4; i++) {
+
+    // For each iteration, generate a new random number between 1 and 9.
+    var random = Math.floor(Math.random() * ) + 1;
+
+    randomNumber = random + randomNumber;
+
+}
